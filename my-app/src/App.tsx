f7a25c00a3e9f67d15c14663227e1c36d56e6ed7
tusharyenurkar <yenurkartushar944@gmail.com>
2025-05-14 12:48:15 +0530

    last commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -19,7 +19,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button 
+          onClick={() => {
+            if (count === 200) {
+              alert("Count is 200");
+              return;
+            }
+            setCount((count) => count + 50);
+          }}
+          disabled={count === 200}
+        >
           count is {count}
         </button>
         <p>
